a760086c8e510a9109ef106cfe057a9be88cb9e4
Maciej Kolodziej <maciej.kolodziej@allegro.pl>
2021-12-02 09:26:59 +0100

    Day 1 done

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,25 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+
+    fun countMeasurementIncreases(input: List<Int>): Int {
+        return input
+            .windowed(2, 1)
+            .map { if(it.last() > it.first()) 1 else 0 }
+            .sum()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun countSumOfSlidingWindow3Increases(input: List<Int>): Int {
+        return countMeasurementIncreases(input
+            .windowed(3, 1)
+            .map { it.sum() }
+        )
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day01_test").map(String::toInt)
+    check(countMeasurementIncreases(testInput) == 7)
+    check(countSumOfSlidingWindow3Increases(testInput) == 5)
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    val input = readInput("Day01").map(String::toInt)
+    println(countMeasurementIncreases(input))
+    println(countSumOfSlidingWindow3Increases(input))
 }
